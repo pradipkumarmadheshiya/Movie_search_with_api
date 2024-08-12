c908eee7725d94a5a0f547cd94b1f24dc9d3bf0c
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2024-08-12 23:10:14 +0530

    search movie with api

---
+++ script.js
@@ -0,0 +1,39 @@
+const moviesBox=document.getElementById("movieBox")
+const input=document.getElementById("input")
+const searchBtn=document.getElementById("btn")
+
+const fetchMovie=async ()=>{
+
+    const apiKey="ad611b1e"
+    const title=input.value
+    
+    try{
+        const res=await fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
+        const data=await res.json()
+        displayMovie(data)
+    }
+    catch(error){
+        console.log("Something went wrong:", error)
+    }
+}
+searchBtn.addEventListener("click", fetchMovie)
+
+const displayMovie=(data)=>{
+    input.value=""
+    data.Search.forEach(el=>{
+        const box=document.createElement("div")
+        box.className="box"
+        const poster=document.createElement("img")
+        const title=document.createElement("p")
+        title.className="title"
+        const releaseYr=document.createElement("p")
+        releaseYr.className="releaseYr"
+
+        poster.src=el.Poster
+        title.textContent=el.Title
+        releaseYr.textContent="Release Date : "+el.Year
+
+        box.append(poster, title, releaseYr)
+        moviesBox.append(box)
+    })
+}
